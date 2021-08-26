2a5520f81261a58e80914106ff4b621b27cbe1d3
Konstantin Läufer <klaeufer@gmail.com>
2021-08-26 15:03:29 -0500

    updated Scaladoc comment for CanEqual instances
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/test/scala/behaviorTests.scala
+++ src/test/scala/behaviorTests.scala
@@ -9,11 +9,11 @@ object behaviorTests extends Properties("behaviorTests") {
   import behaviors._
   import structures._, ExprF._
 
-  /** Enable typesafe equality between `None` and `Option`. */
-  given CanEqual[Option[Int], None.type] = CanEqual.derived
+  /** Enable missing typesafe equality between `None` and `Option`. */
+  given CanEqual[Option[_], None.type] = CanEqual.derived
 
-  /** Enable typesafe equality between `Some` and `Option`. */
-  given CanEqual[Option[Int], Some[Int]] = CanEqual.derived
+  /** Enable missing typesafe equality between `Some` and `Option`. */
+  given [T](using eq: CanEqual[T, T]): CanEqual[Option[T], Some[T]] = CanEqual.derived
 
   val ev = scheme.cata(evaluate)
   property("evaluate1") = Prop { ev(fixtures.complex1) == -1 }
